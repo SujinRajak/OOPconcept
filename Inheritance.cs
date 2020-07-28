fc3f68ae39a2e4ef874d438245e11720e94e1d18
sujin rajak <sujinrajak@gmail.com>
2020-07-28 13:05:47 +0545

    BasicExample of Inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Base{
+        public void show()
+        {
+            Console.WriteLine("hello i am parent class");
+        }
+    }
+    
+    
+    class Inheritance : Base
+    {
+
+        public void derived()
+        {
+            Console.WriteLine("yo bro wassuppp i am his child");
+        }
+        public static void Main()
+        {
+            Inheritance obj = new Inheritance();
+            Base obje = new Base();
+            obje.show();
+            obj.show();
+            obj.derived();
+        }
+        
+    }
+}
