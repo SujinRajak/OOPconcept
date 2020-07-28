9a00ee19a1fd3bf39fa44b64e7afec2c72d10d59
sujin rajak <sujinrajak@gmail.com>
2020-07-28 12:43:09 +0545

    operator overloading

---
+++ OperatorOver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class obj
+    {
+        int a;
+
+        public void getval()
+        {
+            Console.WriteLine("Enter first values:");
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static obj operator -(obj a)
+        {
+            obj val = new obj();
+            val.a = -a.a;
+            return val;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("the number is: {0}", a);
+        }
+    }
+    
+    
+    class OperatorOver
+    {
+        public static void Main()
+        {
+            obj hi = new obj();
+            hi.getval();
+            hi = -hi;
+            hi.print();
+        }
+    
+    }
+}
