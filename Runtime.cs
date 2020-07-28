0c79ea3397784f3e8da7b7ab4d3823681e4989f4
sujin rajak <sujinrajak@gmail.com>
2020-07-28 13:15:46 +0545

    Runtime Example

---
+++ Runtime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Runtime
+    {
+        public static void Main()
+        {
+            // var test = "string";
+            //test = 5;
+
+            dynamic test = "string";
+            test = 5;
+        }
+        
+
+    }
+}
