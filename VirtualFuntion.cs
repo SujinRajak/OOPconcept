8e6217cabfcf377ad1462e5a4e7d92a4a981a8f1
sujin rajak <sujinrajak@gmail.com>
2020-07-28 14:17:46 +0545

    basic of Virtual Funtion

---
+++ VirtualFuntion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Base
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("i am the parent class");
+        }
+    }
+    
+
+    class derived: Base
+    {
+        public override void show()
+        {
+            Console.WriteLine("i am the child class");
+        }
+    }
+    class VirtualFuntion
+    {
+        public static void Main()
+        {
+            derived obj = new derived();
+            Base ob = new Base();
+            ob.show();
+            obj.show();
+        }
+    }
+}
