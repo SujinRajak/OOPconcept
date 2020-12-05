ecdc4a97cc18392cff2bcfecfc2af771c5d764e8
sujin rajak <sujinrajak@gmail.com>
2020-12-05 11:17:37 +0545

    HelloWolrd addded withg pasign parament from console

---
+++ HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if (args == null)
+            {
+                Console.WriteLine("args is null");
+            }
+            else
+            {
+                Console.WriteLine("args length is ");
+                Console.WriteLine(args.Length);
+
+                for(int i =0;i < args.Length; i++)
+                {
+                    string argus = args[i];
+                    Console.Write("args index");
+                    Console.Write(i);
+                    Console.Write("is [");
+                    Console.Write(argus);
+                    Console.WriteLine("]");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
